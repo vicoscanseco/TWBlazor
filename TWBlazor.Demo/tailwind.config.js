e4aa52be7dc0534fb1329205278697fa67cf205c
Victor Canseco <victor.canseco.soto@outlook.com>
2022-02-28 20:39:04 -0600

    Add project files.

---
+++ TWBlazor.Demo/tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+module.exports = {
+    content: [
+        "./src/**/*.{razor,html,cshtml}"
+    ],
+    theme: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+    
+}
---
+++ TWBlazor/TWDropDown.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TWBlazor
+{
+    public partial class TWDropDown
+    {
+        [Parameter]
+        public string Name { get; set; }
+
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public List<(string ItemText, string ItemAction)> Items { get; set; }
+
+        public string DropDownList { get => $"{Name}ddl"; }
+    }
+}
